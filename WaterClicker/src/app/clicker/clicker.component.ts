e9cfb4b8fe6ff8bbf3d7d71c03c4affc7fccc8a7
Hawkfram <52381824+Hawkfram@users.noreply.github.com>
2024-12-05 19:10:43 +0100

    qqq

--- WaterClicker/src/app/clicker/clicker.component.ts
+++ WaterClicker/src/app/clicker/clicker.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-clicker',
@@ -8,5 +8,42 @@ import { Component } from '@angular/core';
   styleUrl: './clicker.component.scss'
 })
 export class ClickerComponent {
+  imageSource = '../../assets/Images/eau.png';
 
+  toggleImage(event: Event): void {
+    const isChecked = (event.target as HTMLInputElement).checked;
+
+    this.imageSource = isChecked
+      ? '../../assets/Images/sang.png'
+      : '../../assets/Images/eau.png';
+  }
+
+  constructor(private renderer: Renderer2) { }
+
+  generateImage(event: MouseEvent): void {
+    const x = event.clientX;
+    const y = event.clientY;
+
+    const newImage = this.renderer.createElement('img');
+    this.renderer.setAttribute(newImage, 'src', '../../assets/Images/eau.png');
+    this.renderer.setStyle(newImage, 'position', 'absolute');
+    this.renderer.setStyle(newImage, 'width', '20px');
+    this.renderer.setStyle(newImage, 'height', '20px');
+    this.renderer.setStyle(newImage, 'top', `${y}px`);
+    this.renderer.setStyle(newImage, 'left', `${x}px`);
+    this.renderer.setStyle(newImage, 'transform', 'translate(-50%, -50%)');
+    this.renderer.setStyle(newImage, 'z-index', '999');
+    this.renderer.setStyle(newImage, 'opacity', '0.8');
+
+    const imageContainer = document.getElementById('image-container');
+    if (imageContainer) {
+      this.renderer.appendChild(imageContainer, newImage);
+
+      setTimeout(() => {
+        this.renderer.removeChild(imageContainer, newImage);
+      }, 2000);
+    }
+  }
 }
+
+
