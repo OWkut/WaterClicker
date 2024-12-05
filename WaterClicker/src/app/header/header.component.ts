142220b7688e9ee2a985a717c0d33196bc9325b0
Hawkfram <52381824+Hawkfram@users.noreply.github.com>
2024-12-06 00:41:42 +0100

    azdazdazdazd

--- WaterClicker/src/app/header/header.component.ts
+++ WaterClicker/src/app/header/header.component.ts
@@ -1,16 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, Renderer2, Inject } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'app-header',
   standalone: true,
   imports: [],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
   showSubmenu: boolean = false;
+  selectedOption: string = 'Francais'; // Valeur par défaut
+  fontFamily: string = 'sans-serif'; // Police par défaut
+
+  constructor(
+    private renderer: Renderer2,
+    @Inject(DOCUMENT) private document: Document
+  ) { }
 
   toggleSubmenu(): void {
     this.showSubmenu = !this.showSubmenu;
+  }
+
+  selectOption(option: string, font: string): void {
+    this.selectedOption = option;
+    this.fontFamily = font;
+    this.applyFontFamily(); // Appliquer la nouvelle police
+    this.showSubmenu = false; // Masquer le sous-menu après sélection
+  }
+
+  applyFontFamily(): void {
+    this.renderer.setStyle(this.document.body, 'font-family', this.fontFamily);
   }
 }
