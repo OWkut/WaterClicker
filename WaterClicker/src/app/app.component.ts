4bab5fd18a3c99179c4eeff6c58c1cd07ad014d8
Florian RIOTTE <riottef@3il.fr>
2024-12-06 01:32:31 +0100

    captcha

--- WaterClicker/src/app/app.component.ts
+++ WaterClicker/src/app/app.component.ts
@@ -2,14 +2,18 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ClickerComponent } from './clicker/clicker.component';
 import { HeaderComponent } from './header/header.component';
+import { CaptchaComponent } from './captcha/captcha.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ClickerComponent, HeaderComponent],
+  imports: [RouterOutlet, ClickerComponent, HeaderComponent, CaptchaComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'WaterClicker';
+  onPopupAction(): void {
+    console.log('Action réalisée dans le pop-up');
+  }
 }
--- WaterClicker/src/app/captcha/captcha.component.ts
+++ WaterClicker/src/app/captcha/captcha.component.ts
@@ -1,15 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { DemineurComponent } from '../demineur/demineur.component';
+import { CommonModule } from '@angular/common';
 
 
 @Component({
   selector: 'app-captcha',
   standalone: true,
-  imports: [DemineurComponent],
+  imports: [DemineurComponent, CommonModule],
   templateUrl: './captcha.component.html',
   styleUrl: './captcha.component.scss'
 })
 export class CaptchaComponent {
+  isVisible = false;
+
+  @Output() action = new EventEmitter<void>();
+
+  openPopup(): void {
+    this.isVisible = true;
+  }
+
+  closePopup(): void {
+    this.isVisible = false;
+  }
+
+  onAction(): void {
+    this.action.emit();
+    this.closePopup();
+  }
+
   isCaptchaValid(): boolean {
     return localStorage.getItem('captchaValidated') === 'true';
   }
